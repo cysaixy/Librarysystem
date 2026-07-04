55c8273712ec9372eca7cc773d146eca01777b51
cysaixy <cyrusdeleon1804@gmail.com>
2026-07-04 15:11:48 +0800

    Clean up comments in RecordList.Designer.vb

--- Librarysystem/RecordList.Designer.vb
+++ Librarysystem/RecordList.Designer.vb
@@ -2,7 +2,7 @@
 Partial Class RecordList
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
+     
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
@@ -14,12 +14,10 @@ Partial Class RecordList
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
+    
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
+     
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblTitle = New System.Windows.Forms.Label()
@@ -46,9 +44,7 @@ Partial Class RecordList
         Me.grp.SuspendLayout()
         CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'lblTitle
-        '
+        
         Me.lblTitle.AutoSize = True
         Me.lblTitle.ForeColor = System.Drawing.Color.White
         Me.lblTitle.Location = New System.Drawing.Point(826, 0)
@@ -56,9 +52,7 @@ Partial Class RecordList
         Me.lblTitle.Size = New System.Drawing.Size(55, 23)
         Me.lblTitle.TabIndex = 1
         Me.lblTitle.Text = "adasd"
-        '
-        'lblCount
-        '
+         
         Me.lblCount.AutoSize = True
         Me.lblCount.ForeColor = System.Drawing.Color.White
         Me.lblCount.Location = New System.Drawing.Point(740, 0)
@@ -66,9 +60,7 @@ Partial Class RecordList
         Me.lblCount.Size = New System.Drawing.Size(80, 23)
         Me.lblCount.TabIndex = 1
         Me.lblCount.Text = "0 records"
-        '
-        'pnlHeader
-        '
+        
         Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(43, Byte), Integer))
         Me.pnlHeader.Controls.Add(Me.Panel1)
         Me.pnlHeader.Controls.Add(Me.lblCount)
@@ -78,9 +70,7 @@ Partial Class RecordList
         Me.pnlHeader.Name = "pnlHeader"
         Me.pnlHeader.Size = New System.Drawing.Size(972, 50)
         Me.pnlHeader.TabIndex = 2
-        '
-        'Panel1
-        '
+        
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(43, Byte), Integer))
         Me.Panel1.Controls.Add(Me.Count)
         Me.Panel1.Controls.Add(Me.txtTitle)
@@ -89,9 +79,7 @@ Partial Class RecordList
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(972, 50)
         Me.Panel1.TabIndex = 3
-        '
-        'Count
-        '
+         
         Me.Count.AutoSize = True
         Me.Count.ForeColor = System.Drawing.Color.White
         Me.Count.Location = New System.Drawing.Point(880, 9)
@@ -99,9 +87,7 @@ Partial Class RecordList
         Me.Count.Size = New System.Drawing.Size(80, 23)
         Me.Count.TabIndex = 1
         Me.Count.Text = "0 records"
-        '
-        'txtTitle
-        '
+        
         Me.txtTitle.AutoSize = True
         Me.txtTitle.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Bold)
         Me.txtTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(199, Byte), Integer), CType(CType(175, Byte), Integer), CType(CType(20, Byte), Integer))
@@ -109,9 +95,7 @@ Partial Class RecordList
         Me.txtTitle.Name = "txtTitle"
         Me.txtTitle.Size = New System.Drawing.Size(0, 24)
         Me.txtTitle.TabIndex = 1
-        '
-        'pnlToolbar
-        '
+         
         Me.pnlToolbar.BackColor = System.Drawing.SystemColors.Control
         Me.pnlToolbar.Controls.Add(Me.GroupBox1)
         Me.pnlToolbar.Controls.Add(Me.btnClose)
@@ -125,72 +109,56 @@ Partial Class RecordList
         Me.pnlToolbar.Name = "pnlToolbar"
         Me.pnlToolbar.Size = New System.Drawing.Size(972, 42)
         Me.pnlToolbar.TabIndex = 3
-        '
-        'GroupBox1
-        '
+        
         Me.GroupBox1.Location = New System.Drawing.Point(0, 44)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(882, 56)
         Me.GroupBox1.TabIndex = 4
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "GroupBox1"
-        '
-        'btnClose
-        '
+        
         Me.btnClose.Location = New System.Drawing.Point(635, 5)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(75, 33)
         Me.btnClose.TabIndex = 9
         Me.btnClose.Text = "Close"
         Me.btnClose.UseVisualStyleBackColor = True
-        '
-        'btnReload
-        '
+       
         Me.btnReload.Location = New System.Drawing.Point(554, 5)
         Me.btnReload.Name = "btnReload"
         Me.btnReload.Size = New System.Drawing.Size(75, 33)
         Me.btnReload.TabIndex = 8
         Me.btnReload.Text = "Reload"
         Me.btnReload.UseVisualStyleBackColor = True
-        '
-        'btnSearch
-        '
+        
         Me.btnSearch.Location = New System.Drawing.Point(473, 5)
         Me.btnSearch.Name = "btnSearch"
         Me.btnSearch.Size = New System.Drawing.Size(75, 33)
         Me.btnSearch.TabIndex = 7
         Me.btnSearch.Text = "Search"
         Me.btnSearch.UseVisualStyleBackColor = True
-        '
-        'btnDelete
-        '
+         
         Me.btnDelete.Location = New System.Drawing.Point(392, 5)
         Me.btnDelete.Name = "btnDelete"
         Me.btnDelete.Size = New System.Drawing.Size(75, 33)
         Me.btnDelete.TabIndex = 6
         Me.btnDelete.Text = "Delete"
         Me.btnDelete.UseVisualStyleBackColor = True
-        '
-        'btnEdit
-        '
+        
         Me.btnEdit.Location = New System.Drawing.Point(309, 5)
         Me.btnEdit.Name = "btnEdit"
         Me.btnEdit.Size = New System.Drawing.Size(75, 33)
         Me.btnEdit.TabIndex = 5
         Me.btnEdit.Text = "Edit"
         Me.btnEdit.UseVisualStyleBackColor = True
-        '
-        'btnAdd
-        '
+         
         Me.btnAdd.Location = New System.Drawing.Point(228, 5)
         Me.btnAdd.Name = "btnAdd"
         Me.btnAdd.Size = New System.Drawing.Size(75, 33)
         Me.btnAdd.TabIndex = 4
         Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = True
-        '
-        'grp
-        '
+        
         Me.grp.Controls.Add(Me.Label3)
         Me.grp.Controls.Add(Me.txtSearch)
         Me.grp.Location = New System.Drawing.Point(0, 94)
@@ -198,9 +166,7 @@ Partial Class RecordList
         Me.grp.Size = New System.Drawing.Size(972, 80)
         Me.grp.TabIndex = 4
         Me.grp.TabStop = False
-        '
-        'Label3
-        '
+        
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 7.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -209,18 +175,14 @@ Partial Class RecordList
         Me.Label3.Size = New System.Drawing.Size(254, 17)
         Me.Label3.TabIndex = 1
         Me.Label3.Text = " Press ENTER to SEARCH and ESC to ESCAPE"
-        '
-        'txtSearch
-        '
+        
         Me.txtSearch.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
         Me.txtSearch.ForeColor = System.Drawing.Color.Gray
         Me.txtSearch.Location = New System.Drawing.Point(228, 20)
         Me.txtSearch.Name = "txtSearch"
         Me.txtSearch.Size = New System.Drawing.Size(508, 30)
         Me.txtSearch.TabIndex = 0
-        '
-        'dgv
-        '
+         
         Me.dgv.AllowUserToAddRows = False
         Me.dgv.AllowUserToDeleteRows = False
         Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
@@ -232,9 +194,7 @@ Partial Class RecordList
         Me.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.dgv.Size = New System.Drawing.Size(972, 329)
         Me.dgv.TabIndex = 5
-        '
-        'RecordList
-        '
+         
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 23.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(232, Byte), Integer))
